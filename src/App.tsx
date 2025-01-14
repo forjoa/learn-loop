4fda20eca3b3c343134b9337a70b05fb6c02c643
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2025-01-14 19:02:01 +0100

    build: init nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { Home, MessageCircle, Settings, Video } from 'lucide-react'
+import { Bell, Book, Home, MessageCircle, Settings } from 'lucide-react'
 
 function App() {
     return (
-        <nav className="flex w-16 flex-col items-center border-r border-medium-grey bg-card px-2 py-4 h-screen">
-            <div className="mb-8">
+        <nav className="flex w-16 flex-col items-center border-r border-medium-grey bg-card px-14 py-8 h-screen">
+            <div className="mb-20">
                 <span
                     className="flex h-10 w-10 items-center justify-center rounded-lg bg-primary text-lg font-semibold text-primary-foreground">
                     <img src="/icon.png" alt="Page icon"/>
                 </span>
             </div>
-            <div className="flex flex-1 flex-col gap-4">
-                <Home className="h-5 w-5"/>
-                <MessageCircle className="h-5 w-5"/>
-                <Video className="h-5 w-5"/>
+            <div className="flex flex-1 flex-col gap-10">
+                <Book className="h-7 w-7" strokeWidth={1.5}/>
+                <Home className="h-7 w-7" strokeWidth={1.5}/>
+                <MessageCircle className="h-7 w-7" strokeWidth={1.5}/>
+                <Bell className="h-7 w-7" strokeWidth={1.5}/>
             </div>
             <div className="mt-auto flex flex-col gap-4">
-                <Settings className="h-5 w-5"/>
+                <Settings className="h-7 w-7" strokeWidth={1.5}/>
             </div>
         </nav>
     )
