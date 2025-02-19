a97c894363af7746ec9447e5545f4c91c07a9ed2
joaquin <joaquin_trujillo@icloud.com>
2025-02-19 16:21:42 +0100

    build: implement manifest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['icon.png'],
+      manifest: {
+        name: 'Learn Loop',
+        short_name: 'LearnLoop',
+        description: 'The best way to learn',
+        theme_color: '#17181A',
+        icons: [
+          {
+            src: 'icon.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     watch: {
-      usePolling: true
+      usePolling: true,
     },
-    host: true
-  }
+    host: true,
+  },
 })
